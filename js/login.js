f23b3db78de6e0cea11fd5a6d10dd1aa69848337
Jonathansoare <jonathansoare90@gmail.com>
2022-08-05 17:49:16 -0300

    main

--- js/login.js
+++
@@ -1,6 +0,0 @@
-function logar() {
-    let btn = document.querySelector('form button.btn')
-        btn.addEventListener("click", () => {
-            location.href = "/views/dashboard.html"
-        })
-}
