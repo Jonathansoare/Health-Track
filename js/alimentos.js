451847d812c7134f07f5f1c47a4bafeefa5a80c5
Jonathan Soares <98005459+Jonathansoare@users.noreply.github.com>
2022-09-30 21:46:24 -0300

    main

--- js/alimentos.js
+++ js/alimentos.js
@@ -1,3 +1,32 @@
-$(() => {
-    
+$(function() {
+
+    $('#time_ative').mask("00:00")
+    $('#data_ative').mask("00/00/0000")
+
+    $("#form-atividade").submit(function (event) {
+        event.preventDefault()
+    }).validate({
+        rules: {
+            ative: {
+                required: true,
+            },
+            time: {
+                required: true,
+            },
+            data: {
+                required: true,
+            },
+        },
+        submitHandler: function (form) {
+            Swal.fire(
+                'Sucesso',
+                'Registro realizado com sucesso ',
+                'success'
+              ).then(() =>{
+                $('#ative').val('')
+                $('#time_ative').val('')
+                $('#data_ative').val('')
+              })
+        }
+    });
 })
