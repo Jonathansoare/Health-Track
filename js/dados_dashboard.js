e8c2002e4712939beba3161bbca211acf0897dac
Jonathan Soares <98005459+Jonathansoare@users.noreply.github.com>
2022-10-29 23:56:47 -0300

    main

--- js/dados_dashboard.js
+++ js/dados_dashboard.js
@@ -4,38 +4,159 @@ function listaTabela(){
     const pesoMylocal = Object.entries(dados)[1][1].peso;
     const alturaMylocal = Object.entries(dados)[1][1].altura;
     const dataMylocal = Object.entries(dados)[1][1].data
-    const porcePesoIndese = document.getElementById('porce-peso-ds')
+    const porcePesoIndese = document.getElementById('porce-peso-ds').innerHTML
     
-    if(pesoMylocal === null){
-        document.getElementById('peso-ds').innerHTML = "00Kg"
-        document.getElementById('porce-peso').innerHTML = "00%"
-        porcePesoIndese.classList.add('fa-ligth fa-angle-up')
-    }
-    else{
-
-    }}
+    boxPeso()
+    boxPressao()
+    boxImc()
+    boxAtividade()
+}
 listaTabela()
 
-/*
-porcentagem()
 
-function porcentagem(){
+function boxPeso(){
     const mylocal = JSON.parse(localStorage.getItem('user'))
     const pesoInput = document.getElementById('peso')
     const peso = pesoInput.value
+    const valuePeso = document.getElementById('value-peso-ds')
+    const indesePorcePesoUp = document.getElementById('porce-peso-ds-up')
+    const indesePorcePesoDn = document.getElementById('porce-peso-ds-dn')
+    const indesePorcePeso = document.getElementById('porce-peso-ds')
+    const valuePorcePeso = document.getElementById('value-porce-peso')
+    const pesoAntigo =  10//Object.entries(mylocal)[1][1].peso;
+    const pesoNovo = 10;
+    const porcentagem = pesoNovo / pesoAntigo - 1
+    const porceFinal = (porcentagem * 100).toFixed(2)
 
-    const pesoAntigo = Object.entries(mylocal)[1][1].peso   
-    const pesoNovo = peso
-
-    if(pesoNovo > pesoAntigo){
-        document.getElementById('porce-peso').innerHTML = Object.entries(mylocal)[18][1] + "%"
-        document.getElementById('porce-peso').style.color = "blue"
-        document.getElementById('porce-peso-ds').style.color = "green"
+    if(pesoAntigo === null || pesoAntigo === 0){
+        valuePorcePeso.innerHTML = "0%"
+        if(pesoNovo === "" || pesoNovo === 0){
+            valuePeso.innerHTML = pesoAntigo + "Kg"
+        }
+        else{
+            valuePeso.innerHTML = pesoNovo + "Kg"
+        }
     }
     else{
-        document.getElementById('porce-peso').innerHTML = Object.entries(mylocal)[18][1] + "%"
-        document.getElementById('porce-peso').style.color = "red"
-        document.getElementById('porce-peso-ds').style.color = "red"
-        document.getElementById('porce-peso-ds').style.transform = "rotate(180deg)"
+        valuePeso.innerHTML = pesoNovo + "Kg"
+        valuePorcePeso.innerHTML = porceFinal + "%"
     }
-}*/
+    if(porceFinal > 0){
+        indesePorcePeso.style.display = "none"
+        indesePorcePesoUp.style.display = "block"
+        valuePorcePeso.style.color = "green"
+    }
+    else if(porceFinal === 0 ){
+        indesePorcePeso.style.display = "block"
+        indesePorcePesoUp.style.display = "none"
+        valuePorcePeso.style.color = "white"
+    }
+    else if(porceFinal < 0){
+        indesePorcePeso.style.display = "none"
+        indesePorcePesoDn.style.display = "block"
+        valuePorcePeso.style.color = "red"
+    }
+}
+
+function boxPressao(){
+    const mylocal = JSON.parse(localStorage.getItem('user'))
+    const pesoInput = document.getElementById('peso')
+    const peso = pesoInput.value
+    const indesePorcePressaoUp = document.getElementById('porce-pressao-ds-up')
+    const indesePorcePressaoDn = document.getElementById('porce-pressao-ds-dn')
+    const indesePorcepressao = document.getElementById('porce-pressao-ds')
+    const valuePressao = document.getElementById('value-pressao-ds')
+    const valuePorcePressao = document.getElementById('value-porce-pressao')
+    const pressaoAntigo =  120//Object.entries(mylocal)[1][1].peso;
+    const pressaoNovo = 121;
+
+    const porcentagem = pressaoNovo / pressaoAntigo - 1
+    const porceFinal = (porcentagem * 100).toFixed(2)
+
+    if(pressaoAntigo === null || pressaoAntigo === 0){
+        valuePorcePressao.innerHTML = "0%"
+        if(pressaoNovo === "" || pressaoNovo === 0){
+            valuePressao.innerHTML = pressaoAntigo + "MMC"
+        }
+        else{
+            valuePressao.innerHTML = pressaoNovo + "MMC"
+
+        }
+    }
+    else{
+        valuePressao.innerHTML = pressaoNovo + "MMC"
+        valuePorcePressao.innerHTML = porceFinal + "%"
+    }
+    if(porceFinal > 0){
+        indesePorcepressao.style.display = "none"
+        indesePorcePressaoUp.style.display = "block"
+        valuePorcePressao.style.color = "green"
+    }
+    else if(porceFinal === 0 ){
+        indesePorcepressao.style.display = "block"
+        indesePorcePressaoUp.style.display = "none"
+        valuePorcePressao.style.color = "white"
+    }
+    else if(porceFinal < 0){
+        indesePorcepressao.style.display = "none"
+        indesePorcePressaoDn.style.display = "block"
+        valuePorcePressao.style.color = "red"
+    }
+}
+
+function boxImc(){
+    const mylocal = JSON.parse(localStorage.getItem('user'))
+    const pesoInput = document.getElementById('peso')
+    const peso = pesoInput.value
+    const indesePorceImcUp = document.getElementById('porce-imc-ds-up')
+    const indesePorceImcDn = document.getElementById('porce-imc-ds-dn')
+    const indesePorceImc = document.getElementById('porce-imc-ds')
+    const valueImc = document.getElementById('value-imc-ds')
+    const valuePorceImc = document.getElementById('value-porce-imc')
+    const imcAntigo =  21.87//Object.entries(mylocal)[1][1].peso;
+    const imcNovo = 21.45;
+    const porcentagem = imcNovo / imcAntigo - 1
+    const porceFinal = (porcentagem * 100).toFixed(2)
+
+    if(imcAntigo === null || imcAntigo === 0){
+        valuePorceImc.innerHTML = "0%"
+        if(imcNovo === "" || imcNovo === 0){
+            valueImc.innerHTML = imcAntigo
+        }
+        else{
+            valueImc.innerHTML = imcNovo
+        }
+    }
+    else{
+        valueImc.innerHTML = imcNovo
+        valuePorceImc.innerHTML = porceFinal
+    }
+    if(porceFinal > 0){
+        indesePorceImc.style.display = "none"
+        indesePorceImcUp.style.display = "block"
+        valuePorceImc.style.color = "green"
+    }
+    else if(porceFinal === 0 ){
+        indesePorceImc.style.display = "block"
+        indesePorceImcUp.style.display = "none"
+        valuePorceImc.style.color = "white"
+    }
+    else if(porceFinal < 0){
+        indesePorceImc.style.display = "none"
+        indesePorceImcDn.style.display = "block"
+        valuePorceImc.style.color = "red"
+    }
+}
+
+function boxAtividade(){
+    const mylocal = JSON.parse(localStorage.getItem('user'))
+    const atividadeMylocal = Object.entries(mylocal)[3][1].tipo
+    const atividade = document.getElementById('atividade-ds')
+    const dataMylocal = Object.entries(mylocal)[3][1].data
+    const data = document.getElementById('date-atividade-ds')
+
+    if(atividadeMylocal === ""){
+        atividade.innerHTML = "Sem atividade"
+        data.innerHTML = "Sem data"
+    }
+}
--- js/peso.js
+++ js/peso.js
@@ -7,16 +7,27 @@ const btn_abri_tela = document.getElementById('button-abrir-cadastro')
 const dataVizul = document.getElementById('date-vizul').innerHTML
 const pesoVizul = document.getElementById('peso-vizul').innerHTML
 const alturaVizul = document.getElementById('altura-vizul').innerHTML
-const btn_abri_tela_vizul = document.getElementById('button-abrir-vizul')
+//const btn_abri_tela_vizul = document.getElementById('button-abrir-vizul')
 
 
 
 btn.addEventListener("click", (el) => {
     el.preventDefault()
-    const peso = pesoInput.value
-    const altura = alturaInput.value
+    const peso = Number(pesoInput.value)
+    const altura = Number(alturaInput.value)
+
+    if(peso === ""){
+        mostraMsg("Peso invalido",'msg-erro')
+    }
+    else if(altura === ""){
+        mostraMsg("Altura invalida",'msg-erro')
+    }
+    else{
+        cadastraPeso(peso,altura)
+    }
 
 })
+
 
 function listaTabela(){
     // [0]-dadosPessoais [1]-dadosPeso [2]-pressao [3]-atividade [4]-alimentos [5]-dashboard
@@ -70,12 +81,20 @@ function AmostraFormVizul(){
             listaTabela()
         }}}
 
-function mostraMsg(y,a){
+function mostraMsg(a,y){
     document.getElementById('alert-msg').innerHTML = a
-    document.getElementById('alert-msg').style.color = y
+    document.getElementById('alert-msg').classList.remove('msg-erro'|| 'msg')
+    document.getElementById('alert-msg').classList.add(y)
+    document.getElementById('alert-msg').style.transition = '0.2s'
     setTimeout(() => {
-        mostraMsg("","")
-    }, 7000);
+        document.getElementById('alert-msg').innerHTML = ""
+        document.getElementById('alert-msg').classList.remove('msg-erro' && 'msg')
+        document.getElementById('alert-msg').style.transition = '0.2s'
+    }, 5000);
+}
+function erroInputs(a,y){
+    a.classList.remove("input-register-erro" || "input-register")
+    a.classList.add(y)
 }
 
 function editar(){
@@ -94,84 +113,94 @@ function editar(){
 }
 
 function apagar(){  
+
 }
-function calcularIMC(x,y) {
+
+function calcularIMC(y,x) {
     const mylocal = JSON.parse(localStorage.getItem('user'));
     const imc = y / (x * x)
-    const resul = imc.toFixed(2)
-    const imc_indese = ""
-    
-    
-    if(imc < 18.6){
-        const user_dados = JSON.stringify({
-            email:Object.entries(mylocal)[0][1],
-            senha:Object.entries(mylocal)[1][1],
-            nome:Object.entries(mylocal)[2][1],
-            idade:Object.entries(mylocal)[3][1],
-            genero:Object.entries(mylocal)[4][1],
-            peso: Object.entries(mylocal)[5][1],
-            altura: Object.entries(mylocal)[6][1],
-            data_peso: Object.entries(mylocal)[7][1],
-            pressao:Object.entries(mylocal)[8][1],
-            data_pressao:Object.entries(mylocal)[9][1],
-            atividade:Object.entries(mylocal)[10][1],
-            time_atividade:Object.entries(mylocal)[11][1],
-            data_atividade:Object.entries(mylocal)[12][1],
-            data_alimento:Object.entries(mylocal)[13][1],
-            alimentos:Object.entries(mylocal)[14][1],
-            calorias:Object.entries(mylocal)[15][1],
-            IMC:resul,
-            imc_indese: "Abaixo do peso",
-            porcentagem_peso: Object.entries(mylocal)[18][1],
-        })
-        localStorage.setItem('user',user_dados)
-    }
-    else if(imc >= 18.6 && imc < 24.9){
-        const user_dados = JSON.stringify({
-            email:Object.entries(mylocal)[0][1],
-            senha:Object.entries(mylocal)[1][1],
-            nome:Object.entries(mylocal)[2][1],
-            idade:Object.entries(mylocal)[3][1],
-            genero:Object.entries(mylocal)[4][1],
-            peso: Object.entries(mylocal)[5][1],
-            altura: Object.entries(mylocal)[6][1],
-            data_peso: Object.entries(mylocal)[7][1],
-            pressao:Object.entries(mylocal)[8][1],
-            data_pressao:Object.entries(mylocal)[9][1],
-            atividade:Object.entries(mylocal)[10][1],
-            time_atividade:Object.entries(mylocal)[11][1],
-            data_atividade:Object.entries(mylocal)[12][1],
-            data_alimento:Object.entries(mylocal)[13][1],
-            alimentos:Object.entries(mylocal)[14][1],
-            calorias:Object.entries(mylocal)[15][1],
-            IMC:resul,
-            imc_indese: "Normal",
-            porcentagem_peso: Object.entries(mylocal)[18][1],
-        })
-        localStorage.setItem('user',user_dados)
-    }
-    else{
-        const user_dados = JSON.stringify({
-        email:Object.entries(mylocal)[0][1],
-        senha:Object.entries(mylocal)[1][1],
-        nome:Object.entries(mylocal)[2][1],
-        idade:Object.entries(mylocal)[3][1],
-        genero:Object.entries(mylocal)[4][1],
-        peso: Object.entries(mylocal)[5][1],
-        altura: Object.entries(mylocal)[6][1],
-        data_peso: Object.entries(mylocal)[7][1],
-        pressao:Object.entries(mylocal)[8][1],
-        data_pressao:Object.entries(mylocal)[9][1],
-        atividade:Object.entries(mylocal)[10][1],
-        time_atividade:Object.entries(mylocal)[11][1],
-        data_atividade:Object.entries(mylocal)[12][1],
-        data_alimento:Object.entries(mylocal)[13][1],
-        alimentos:Object.entries(mylocal)[14][1],
-        calorias:Object.entries(mylocal)[15][1],
-        IMC:resul,
-        imc_indese: "Excesso de peso",
-        porcentagem_peso: Object.entries(mylocal)[18][1],
-    })
-    localStorage.setItem('user',user_dados)
-    }
+    const imcResul = Number(imc.toFixed(2))
+   
+    return imcResul
 }
+
+function indeseImc(imc){
+    const nivel = ['Abaixo do Peso','peso normal','sobrepeso','Obesidade grau 1','Obesidade grau 2','Obesidade grau 3']
+
+    if(imc >= 39.9) return nivel[5];
+    if(imc >= 34.9) return nivel[4];
+    if(imc >= 29.9) return nivel[3];
+    if(imc >= 24.9) return nivel[2];
+    if(imc >= 18.5) return nivel[1];
+    if(imc < 18.5) return nivel[0];
+    
+}
+
+function porcentagemImc(peso,altura){
+    const mylocal = JSON.stringify(localStorage.getItem('user'))
+    const imcAntigo = Object.entries(mylocal)[4][1].imc;
+    if(imcAntigo === undefined || imcAntigo === 0){
+        console.log("nao tem no db");
+        return 0
+    }
+    else if (imcAntigo > 0){
+        console.log(imcAntigo);
+        const imcNovo = calcularIMC(peso,altura);
+        console.log(imcNovo);
+        const porcentagem = imcNovo / imcAntigo - 1
+        console.log(porcentagem);
+        const porceFinal = (porcentagem * 100).toFixed(2)
+        console.log(Number(porceFinal));
+        return porceFinal
+    }
+    
+
+}
+
+function cadastraPeso(peso,altura){
+    const mylocal = JSON.stringify(localStorage.getItem('user'))
+    const data = new Date();
+    let dia = data.getDate()
+    let ano = data.getFullYear()
+    let diaMes = data.getMonth()
+    // [0]-dadosPessoais [1]-dadosPeso [2]-pressao [3]-atividade [4]-alimentos [5]-dashboard
+    const user = JSON.stringify({
+        dadosPessoais:{
+            nome:Object.entries(mylocal)[0][1].nome,
+            email:Object.entries(mylocal)[0][1].email,
+            idade:Object.entries(mylocal)[0][1].idade,
+            genero:Object.entries(mylocal)[0][1].genero,
+            senha:Object.entries(mylocal)[0][1].senha
+        },
+        dadosPeso:{
+            peso:peso,
+            altura:altura,
+            data:dia+"/"+diaMes+"/"+ano,
+        },
+        pressao:{
+            data:Object.entries(mylocal)[2][1].data,
+            pressao:Object.entries(mylocal)[2][1].pressao,
+        },
+        atividade:{
+            data:Object.entries(mylocal)[3][1].data,
+            tipo:Object.entries(mylocal)[3][1].tipo,
+            duracao:Object.entries(mylocal)[3][1].duracao,
+        },
+        alimento:{
+            data: Object.entries(mylocal)[4][1].data,
+            ultimo_alimento: Object.entries(mylocal)[4][1].ultimo_alimento,
+            calorias: Object.entries(mylocal)[4][1].calorias,
+        },
+        dashboard:{
+            porcentagem_peso: Object.entries(mylocal)[5][1].porcentagem_peso,
+            porcentagem_pressao: Object.entries(mylocal)[4][1].porcentagem_pressao,
+            imc: calcularIMC(peso,altura),
+            porcentagem_imc:porcentagemImc(peso,altura),
+            indese:indeseImc(calcularIMC(peso))
+        }})
+    localStorage.setItem('user',user)
+    mostraMsg("Peso cadastrado com sucesso!",'msg')
+    setTimeout(() => {
+        AmostraFormVizul()
+    }, 3000);
+    }
